dac74035b07114e02cc64681f8717795d2770e7b
George Jopson <georgejopson@gmail.com>
2020-12-28 16:02:23 +0000

    Complete basic functionality

--- app.js
+++ app.js
@@ -9,12 +9,40 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.get("/",function(req,res){
-  let message="Hello!"
-  res.render("home",{message:message});
+  let message="Hello!";
+  let freindsResult=null;
+  let action="createLink";
+  res.render("home",{message:message,freindsResult:freindsResult,action:action});
 });
 
 app.post("/createLink",function(req,res){
-  console.log(req.body.biscuits+req.body.colour+req.body.food)
+  let results = req.body.biscuits+req.body.colour+req.body.food;
+  let link = "localhost:3000/"+results;
+  res.render("results",{output:link});
+});
+
+app.get("/:results",function(req,res){
+  let freindsResult = req.params.results;
+  let message ="Guess your freinds answers";
+  let action ="score";
+  res.render("home",{message:message,freindsResult:freindsResult,action:action});
+});
+
+app.post("/score",function(req,res){
+  let results = (req.body.biscuits+req.body.colour+req.body.food).split("");
+  let freindsResult = req.body.freindsResult.split("");
+  console.log(results);
+  console.log(freindsResult);
+  let correct=0;
+  let outOf=0
+  results.forEach(function(answer){
+    if (answer==freindsResult[outOf]) {
+      correct++;
+    }
+    outOf++;
+  });
+  let score = correct+"/"+outOf;
+  res.render("results",{output:score})
 });
 
 let port = process.env.PORT;
@@ -23,5 +51,5 @@ if (port == null || port == "") {
 }
 
 app.listen(port,function(){
-  console.log("Server started")
+  console.log("Server started");
 });
